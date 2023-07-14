45c96ed258dc99d8b36eb24b13ddf66a5e37f166
ThisasGamage <22465@wc.school.nz>
2023-07-15 02:51:36 +1200

    Player stats - Started

---
+++ MechXAliens/Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    [SerializeField] protected int health;
+    [SerializeField] protected int maxHealth;
+
+    [SerializeField] protected bool isDead;
+
+    public void CheckHealth()
+    {
+        if (health <= 0)
+        {
+            health = 0;
+            Die();
+        }
+        if (health >= maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    public void Die()
+    {
+        isDead = true;
+    }
+
+    public void SetHealthTo(int healthToSetTo)
+    {
+        health = healthToSetTo;
+    }
+}
