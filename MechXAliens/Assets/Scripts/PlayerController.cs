f69a70734b5d4815f1449fc961b3757806cd0063
ThisasGamage <22465@wc.school.nz>
2023-07-15 02:41:13 +1200

    Player Controller

---
+++ MechXAliens/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Move Variables")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float jumpForce;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    [Header("Gravity")]
+    [SerializeField] private float gravity;
+    [SerializeField] private float groundDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private bool isCharacterGrounded = false;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        GetReferences();
+        InitVariables();
+    }
+
+    private void Update()
+    {
+        HandleIsGrounded();
+        HandleJumping();
+        HandleGravity();
+        
+
+        HandleRunning();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector3(moveX, 0, moveZ);
+        moveDirection = moveDirection.normalized;
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    private void HandleRunning()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveSpeed = runSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed = walkSpeed;
+        }
+    }
+
+    private void HandleIsGrounded()
+    {
+        isCharacterGrounded = Physics.CheckSphere(transform.position, groundDistance);
+    }
+
+    private void HandleJumping() 
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isCharacterGrounded)
+        {
+            velocity.y += Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
+    }
+
+    private void HandleGravity()
+    {
+        if (isCharacterGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void GetReferences()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void InitVariables()
+    {
+        moveSpeed = walkSpeed;
+    }
+}
