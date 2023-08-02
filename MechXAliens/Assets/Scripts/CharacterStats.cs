9504dc30535b4c056ecebee9b415ce56302bf4c9
ThisasGamage <22465@wc.school.nz>
2023-08-03 11:40:10 +1200

    Created a health system

--- MechXAliens/Assets/Scripts/CharacterStats.cs
+++ MechXAliens/Assets/Scripts/CharacterStats.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
@@ -9,7 +10,12 @@ public class CharacterStats : MonoBehaviour
 
     [SerializeField] protected bool isDead;
 
-    public void CheckHealth()
+    private void Start()
+    { 
+        InitVariable();
+    }
+
+    public virtual void CheckHealth()
     {
         if (health <= 0)
         {
@@ -30,5 +36,25 @@ public class CharacterStats : MonoBehaviour
     public void SetHealthTo(int healthToSetTo)
     {
         health = healthToSetTo;
+        CheckHealth();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        int healthAfterDamage = health - damage;
+        SetHealthTo(healthAfterDamage);
+    }
+
+    public void Heal(int heal)
+    { 
+        int healthAfterDamage = health + heal;
+        SetHealthTo(healthAfterDamage);
+    }
+
+    public void InitVariable()
+    {
+        maxHealth = 100;
+        SetHealthTo(maxHealth);
+        isDead = false;
     }
 }
