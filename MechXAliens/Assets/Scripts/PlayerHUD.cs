725d85c9f51ccf24b061dcef563ac115c8fff4d2
ThisasGamage <22465@wc.school.nz>
2023-08-26 02:16:00 +1200

    Health bar done

--- MechXAliens/Assets/Scripts/PlayerHUD.cs
+++ MechXAliens/Assets/Scripts/PlayerHUD.cs
@@ -5,12 +5,10 @@ using UnityEngine.UI;
 
 public class PlayerHUD : MonoBehaviour
 {
-    [SerializeField] private Text currentHealthText;
-    [SerializeField] private Text maxHealthText;
+    [SerializeField] private ProgressBar healthbar;
 
     public void UpdateHealth( int currentHealth, int maxHealth)
     {
-        currentHealthText.text = currentHealth.ToString();
-        maxHealthText.text = maxHealth.ToString();
+        healthbar.SetValues(currentHealth, maxHealth);
     }
 }
